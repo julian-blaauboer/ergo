0e3df1dfbeb928e1fa44828fd4878ce9b5a9ae1e
Julian Blaauboer <julian.blaauboer@gmail.com>
2020-02-01 13:11:59 +0100

    Initial commit

---
+++ examples/socrates.rs
@@ -0,0 +1,36 @@
+extern crate ergo;
+
+fn main() {
+    // For readability of terms.
+    use ergo::{compound, atom, variable};
+
+    // Create a new universe.
+    let mut uni = ergo::resolution::Universe::new();
+
+    // Add some facts and rules to the universe
+    // The equivalent prolog clauses are:
+    // human(socrates).
+    // human(plato).
+    // mortal(X) :- human(X).
+    uni.add_fact(compound!("human": atom("socrates")));
+    uni.add_fact(compound!("human": atom("plato")));
+    uni.add_rule(compound!("mortal": variable("X")), vec![
+        compound!("human": variable("X"))
+    ]);
+
+    // Query the universe
+    let results = uni.query(vec![
+        compound!("mortal": variable("X"))
+    ]);
+
+    // Universe::query returns an iterator.
+    // Queries are lazily evaluated.
+    for result in results {
+        // Instantiate the variable from the instantiations
+        let x = ergo::unification::instantiate(variable("X"), &result);
+        println!("X = {:?}", x);
+        // Should output:
+        // X = Atom("socrates")
+        // X = Atom("plato")
+    }
+}
